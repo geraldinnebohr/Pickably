1981501b144ee59e65f5ef13311479f952d263c3
julgachancipa <julianaga01@gmail.com>
2020-03-03 14:10:27 -0500

    [FEATURE] - room end points

--- backend/models/room.model.js
+++ backend/models/room.model.js
@@ -1,39 +1,43 @@
-// const mongoose = require('mongoose');
-// const ShortId = require('shortid');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const shortid = require('shortid');
 
-// const Schema = mongoose.Schema;
+const playerSchema = new Schema(
+    {
+        userName: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        score: {
+            type: Number,
+            default: 0 
+        }
+    },
+    {
+    timestamps: true
+    }
+);
 
-// const playerSchema = new Schema({
-//   userName: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//     trim: true
-//   },
-//   score: {
-//     type: Number,
-//     default: 0 
-//   }
-// });
 
-// const roomSchema = new Schema(
-//   {
-//     _id: {
-//       type: String,
-//       default: ShortId.generate
-//     },
-//     qtryId: {
-//       type: String,
-//       required: true
-//     },
-//     players: [playerSchema]
-//   },
-//   {
-//   // support date time format
-//   timestamps: true
-//   }
-// );
+const roomSchema = new Schema(
+    {
+        _id: {
+            'type': String,
+            'default': shortid.generate
+        },
+        qtryId: {
+            type: String,
+            required: true
+        },
+        players: [playerSchema]
+    },
+    {
+    // support date time format
+    timestamps: true
+    }
+);
 
-// const Room = mongoose.model('Room', roomSchema);
+const Room = mongoose.model('Room', roomSchema);
 
-// module.exports = Room;
+module.exports = Room;
