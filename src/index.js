df185240e060fa166c7bbe9c0cef49a4db04e1dc
julgachancipa <julianaga01@gmail.com>
2020-03-11 00:32:15 -0500

    [FEATURE] - socket io first commit

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import 'bootstrap/dist/css/bootstrap.css';
-
-import App from './components/App';
-
-const container = document.getElementById('App');
-
-ReactDOM.render(<App />, container);
