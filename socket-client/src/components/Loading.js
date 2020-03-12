f53b14688c300508e72221f3578cfee0541a5e67
geraldinnebohr <geraldinnebohr@gmail.com>
2020-03-12 15:50:26 -0500

    [FEATURE] loading component created

---
+++ socket-client/src/components/Loading.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import BackgroundDark from './BackgroundDark';
+
+class Loading extends React.Component {
+    render() {
+        return (
+            <div className="grid_container_dark">
+                <div className="child__content__answer">
+                    prueba
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Loading;
