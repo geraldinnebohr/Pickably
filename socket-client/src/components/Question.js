9d3f6bfbe17691e2cb84a1f51cd2cb43b37fb55c
geraldinnebohr <geraldinnebohr@gmail.com>
2020-03-12 19:47:43 -0500

    [FEATURE] css question

--- socket-client/src/components/Question.js
+++ socket-client/src/components/Question.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './Styles/Question.css'
+
 class Question extends React.Component {
     state = {
         loading: true,
@@ -35,11 +37,15 @@ class Question extends React.Component {
         
         
         return (
-            <div>
-                <div>
-                    <div>
-                        <p>{this.state.data.description}</p>
+            <div className="grid_container_dark">
+                <div className="child__content__answer">
+                    <div className="counter__left">
+                        <div className="counter__circle">
+                            {/* <span className="box firstNumber"></span> */}
+                            <span className="box secondNumber"></span>
+                        </div>
                     </div>
+                    <div className="question__right">{this.state.data.description}</div>
                 </div>
             </div>
         )
