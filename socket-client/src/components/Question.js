ea1e8f24351272f87ee84039eb0915dcd636fb37
julgachancipa <julianaga01@gmail.com>
2020-03-12 21:36:37 -0500

    [FEATURE] - question slides

--- socket-client/src/components/Question.js
+++ socket-client/src/components/Question.js
@@ -5,7 +5,7 @@ class Question extends React.Component {
     state = {
         loading: true,
         error: null,
-        index: 0,
+        index: null,
         data: [ ],
         redirect: false
     };
@@ -13,26 +13,24 @@ class Question extends React.Component {
     // sending sockets
     send = (i) => {
         const socket = socketIOClient("localhost:5500");
-        socket.emit('please answer', i);
+        socket.emit('time to vote', i);
     }
 
     componentDidMount() {
         this.fetchData();
-        this.id = setTimeout(() => {
+        setTimeout(() => {
             this.send(this.state.index);
-            window.location.href='./answers';
-        }, 10000)
-    }
-
-    componentWillUnmount() {
-        clearTimeout(this.id)
-    }    
+            window.location.href='./answers?index=' + this.state.index;
+        }, 5000)
+    }  
 
     fetchData = async () => {
-            this.setState({ loading: true, error: null });
-
+            const search = window.location.search;
+            const params = new URLSearchParams(search);
+            const i = params.get('index');
+            this.setState({ loading: true, error: null, index: i });
             try {
-                const response = await fetch("http://localhost:5500/questionary/5e44e0a71c9d440000177bf7/question/" + this.state.index);
+                const response = await fetch("http://localhost:5500/questionary/5e44e0a71c9d440000177bf7/question/" + i);
                 const data = await response.json();
                 this.setState({ loading: false, data: data });
             } catch (error) {
